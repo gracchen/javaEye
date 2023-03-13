bb8ca0432b63e01d4bc5509b869c1ade469f7276
gracchen <gracetheartist@g.ucla.edu>
2023-03-12 17:58:24 -0700

    Add settings tab & sliders

--- src/try1/GUI.java
+++ src/try1/GUI.java
@@ -15,8 +15,13 @@ import java.time.LocalDateTime;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class GUI extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -28,19 +33,28 @@ public class GUI extends JFrame {
 	SystemTray tray;
 	GridBagConstraints c;
 	String alarmMsg, breakMsg, workMsg;
+	JTabbedPane tabPane;
+	JPanel home, settings;
+	JLabel tab1, tab2;
+	
+	//settings elements:
+	JLabel workSliderLabel, breakSliderLabel, seeWork, seeBreak;
+	JSlider sliderWork, sliderBreak;
+	
 	boolean isHidden;
 	
 	public GUI(int pollTime) {
 		super("Eye Saver");
-		setLayout(new GridBagLayout()); //default
 		pack();
 		setLocationRelativeTo(null);
 		this.pollTime = pollTime / 1000 + 5; //convert to sec with leeway of 5 sec lag
-		workMin = 1; breakSec = 20;
+		workMin = 20; breakSec = 20;
 		alarmMsg = "ALARM: ";
 		breakMsg = "HOORAY!! Break!";
 		workMsg = "Hello :)";
 		
+		home = new JPanel();
+		home.setLayout(new GridBagLayout()); //more customizable gui layout
 		msg = new JLabel(workMsg, SwingConstants.CENTER); 
 		now = LocalDateTime.now(); //Date.setTime
 		alarm = now.plusMinutes(workMin);
@@ -57,29 +71,83 @@ public class GUI extends JFrame {
 		}});
 		
 		c = new GridBagConstraints(); 
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 2;
-		c.gridx = 0; c.gridy = 0;
-		add(msg, c);
-		c.insets = new Insets(8,1,8,1);
+		c.fill = GridBagConstraints.HORIZONTAL;    //fill entire cell with text to center
+		c.gridwidth = 2; c.gridx = 0; c.gridy = 0;   //coords + width of msg element
+		home.add(msg, c); //apply to msg and add to home panel
+		c.insets = new Insets(8,1,8,1);  //y,x,y,x padding
 
-		c.gridx = 0; c.gridy = 1;
-		add(endText, c);
+		c.gridwidth = 2; c.gridx = 0; c.gridy = 1;
+		home.add(endText, c);
 		
-		c.gridwidth = 1;
-		c.fill = GridBagConstraints.NONE;
-		c.gridx = 0; c.gridy = 2;
-		add(reset, c);
+		c.gridwidth = 1; c.gridx = 0; c.gridy = 2;
+		c.fill = GridBagConstraints.NONE; 	//keep buttons small
+		home.add(reset, c);
 
 		if(SystemTray.isSupported())
 		{
-			c.gridx = 1; c.gridy = 2;
-			add(hide, c);
+			c.gridx = 1; c.gridy = 2;  //put in same row as other button
+			home.add(hide, c);
 			tray = SystemTray.getSystemTray();
 			trayIcon = new TrayIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/icon.png")));
 			trayIcon.addMouseListener(new click());
-			trayIcon.setToolTip("Right click to exit");
+			trayIcon.setToolTip("Right click to exit"); //appear on hovering icon
 		}
+		
+		//Now setting window:
+		settings = new JPanel();
+		settings.setLayout(new GridBagLayout());
+		
+		//work duration setting:
+		workSliderLabel = new JLabel("Work duration (min):", SwingConstants.LEFT);
+		c.insets = new Insets(1,1,1,1); //less padding than in home
+		c.gridwidth = 2; c.gridx = 0; c.gridy = 0;
+		c.anchor = GridBagConstraints.WEST;  //align setting label to left
+		settings.add(workSliderLabel, c);
+		seeWork = new JLabel(String.valueOf(workMin), SwingConstants.LEFT);
+		sliderWork = new JSlider(SwingConstants.HORIZONTAL, 1, 60, 10); //range: 1 to 60 min
+		sliderWork.setValue(workMin);  //start by reflecting default
+		sliderWork.addChangeListener(
+				new ChangeListener() {
+					public void stateChanged(ChangeEvent e) {
+						workMin = sliderWork.getValue();   //set value
+						seeWork.setText(String.valueOf(workMin));  //show curr value
+					}
+				}
+		);
+		c.anchor = GridBagConstraints.CENTER;  //center slider
+		c.gridwidth = 1; c.gridx = 0; c.gridy = 1;
+		settings.add(sliderWork,c);
+		c.gridwidth = 1; c.gridx = 1; c.gridy = 1;
+		settings.add(seeWork, c);
+		
+		
+		//break duration setting:
+		breakSliderLabel = new JLabel("Break duration (sec):", SwingConstants.LEFT);
+		c.gridwidth = 2; c.gridx = 0; c.gridy = 2;
+		c.anchor = GridBagConstraints.WEST;
+		settings.add(breakSliderLabel, c);
+		seeBreak = new JLabel(String.valueOf(breakSec), SwingConstants.LEFT);
+		sliderBreak = new JSlider(SwingConstants.HORIZONTAL, 1, 60, 10);
+		sliderBreak.setValue(breakSec);
+		sliderBreak.addChangeListener(
+				new ChangeListener() {
+					public void stateChanged(ChangeEvent e) {
+						breakSec = sliderBreak.getValue();
+						seeBreak.setText(String.valueOf(breakSec));
+					}
+				}
+		);
+		c.anchor = GridBagConstraints.CENTER;
+		c.gridwidth = 1; c.gridx = 0; c.gridy = 3;
+		settings.add(sliderBreak,c);
+		c.gridwidth = 1; c.gridx = 1; c.gridy = 3;
+		settings.add(seeBreak, c);
+		
+		//tabs
+		tabPane = new JTabbedPane();
+		tabPane.addTab("Home", home);
+		tabPane.addTab("Settings", settings);
+		add(tabPane);
 	}
 	
 	private class click extends MouseAdapter {
@@ -123,13 +191,14 @@ public class GUI extends JFrame {
 				reset.setVisible(false);
 				hide.setVisible(false);
 				endText.setVisible(false);
+				
 				hackyToFront();
 				resetAlarm();
-				endText.setVisible(true);
-				endText.setText(alarmMsg + makeString(alarm));
+				
 				msg.setText(workMsg);
-				hide.setVisible(true);
 				reset.setVisible(true);
+				hide.setVisible(true);
+				endText.setVisible(true);
 			}
 		}
 		else //computer sleeped, reset alarm
@@ -153,8 +222,7 @@ public class GUI extends JFrame {
 			e.printStackTrace();
 		}
 		setAlwaysOnTop(false);
-		if (isHidden)
-			setVisible(false);
+		if (isHidden) setVisible(false);
 		setExtendedState(JFrame.ICONIFIED);
 		toBack();
 	}
